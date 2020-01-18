91992db35ca64dec3af684c0434381fbc8044c72
Jabez123 <it.jabez16@gmail.com>
2020-01-18 15:52:03 +0800

    Fix big space near footer section, trying 1 content only

--- index.php
+++ index.php
@@ -14,109 +14,89 @@
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-dark unique-color">
 			<div class="container">
-				<a class="navbar-brand" href="#">Rendering System</a>
+				<a class="navbar-brand" id="topSection" href="#">Rendering System</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Trainees</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">In Charge</a>
-					</li>
-				</ul>
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<span class="navbar-text white-text mr-5">
-							Hello Admin!
-						</span>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-						aria-haspopup="true" aria-expanded="false">
-						<i class="fas fa-user"></i>
-					</a>
-					<div class="dropdown-menu dropdown-menu-left"
-					aria-labelledby="navbarDropdownMenuLink-333">
-					<a class="dropdown-item" href="#">Change Password</a>
-					<a class="dropdown-item" href="#">Logout</a>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Trainees</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">In Charge</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<span class="navbar-text white-text mr-5">
+								Hello Admin!
+							</span>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+								aria-haspopup="true" aria-expanded="false">
+								<i class="fas fa-user"></i>
+							</a>
+							<div class="dropdown-menu dropdown-menu-left"
+							aria-labelledby="navbarDropdownMenuLink-333">
+								<a class="dropdown-item" href="#">Change Password</a>
+								<a class="dropdown-item" href="#">Logout</a>
+							</div>
+						</li>
+					</ul>
 				</div>
+			</div>
+		</nav>
+	</header>
+
+	<main class="mt-5">
+
+		<ul class="nav nav-tabs" id="dashboardTab" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link active" id="trainees-tab" data-toggle="tab" href="#trainees" role="tab" aria-controls="trainees"
+				aria-selected="true">Trainees Dashboard</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" id="incharge-tab" data-toggle="tab" href="#incharge" role="tab" aria-controls="profile"
+				aria-selected="false">In-charge Dashboard</a>
 			</li>
 		</ul>
-	</div>
-</div>
-</nav>
-</header>
+		<div class="tab-content" id="dashboardTabContent">
+			<div class="tab-pane fade show active" id="trainees" role="tabpanel" aria-labelledby="trainees-tab">
 
-<main class="mt-5">
+				<div class="container" style="margin-top: 80px;">
+					<div class="row">
+						<div class="col-12">
+							<!-- Card Dark -->
+							<div class="card">
+								<!-- Card content -->
+								<div class="card-body elegant-color white-text rounded-bottom">
+									<!-- Title -->
+									<h4 class="card-title text-center"> Trainees Dashboard</h4>
+									<hr class="hr-light">
+									<!-- Body -->
+									<div class="container-fluid">
+										<div class="row">
+											<div class="col-sm-12">
 
-	<ul class="nav nav-tabs" id="dashboardTab" role="tablist">
-		<li class="nav-item">
-			<a class="nav-link active" id="trainees-tab" data-toggle="tab" href="#trainees" role="tab" aria-controls="trainees"
-			aria-selected="true">Trainees Dashboard</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" id="incharge-tab" data-toggle="tab" href="#incharge" role="tab" aria-controls="profile"
-			aria-selected="false">In-charge Dashboard</a>
-		</li>
-	</ul>
-	<div class="tab-content" id="dashboardTabContent	">
-		<div class="tab-pane fade show active" id="trainees" role="tabpanel" aria-labelledby="trainees-tab">
-
-			<div class="container" style="margin-top: 80px;">
-				<div class="row">
-					<div class="col-12">
-						<!-- Card Dark -->
-						<div class="card">
-							<!-- Card content -->
-							<div class="card-body elegant-color white-text rounded-bottom">
-								<!-- Title -->
-								<h4 class="card-title text-center"> Trainees Dashboard</h4>
-								<hr class="hr-light">
-								<!-- Body -->
-								<div class="container-fluid">
-									<div class="row">
-										<div class="col-sm-12">
-
-											<div class="card mb-4">
-												<div class="card-body">
-													<!-- Material input -->
-													<div class="md-form">
+												<div class="card mb-4">
+													<div class="card-body">
+														<!-- Material input -->
+														<div class="md-form">
 														<input type="text" id="form1" class="form-control">
 														<label for="form1">Search</label>
+														</div>
+														<div class="text-center">
+															<button type="button" class="btn btn-primary">Search</button>
+														</div>
 													</div>
-													<div class="text-center">
-														<button type="button" class="btn btn-primary">Search</button>
-													</div>
-
 												</div>
 											</div>
-
-										</div>
-										<div class="col-sm-4">
-											<!-- Card -->
-											<div class="card mb-4">
-												<!--Card content-->
-												<div class="card-body">
-
-													<!--Title-->
-													<h4 class="card-title">Card title</h4>
-													<!--Text-->
-													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-													<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-													<button type="button" class="btn btn-light-blue btn-md">Read more</button>
-
-												</div>
-
-											</div>
-											<!-- Card -->
-										</div>
 
 										<div class="col-sm-4">
 											<!-- Card -->
@@ -136,83 +116,6 @@
 											</div>
 											<!-- Card -->
 										</div>
-
-										<div class="col-sm-4">
-											<!-- Card -->
-											<div class="card mb-4">
-												<!--Card content-->
-												<div class="card-body">
-
-													<!--Title-->
-													<h4 class="card-title">Card title</h4>
-													<!--Text-->
-													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-													<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-													<button type="button" class="btn btn-light-blue btn-md">Read more</button>
-
-												</div>
-
-											</div>
-											<!-- Card -->
-										</div>
-
-										<div class="col-sm-4">
-											<!-- Card -->
-											<div class="card mb-4">
-												<!--Card content-->
-												<div class="card-body">
-
-													<!--Title-->
-													<h4 class="card-title">Card title</h4>
-													<!--Text-->
-													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-													<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-													<button type="button" class="btn btn-light-blue btn-md">Read more</button>
-
-												</div>
-
-											</div>
-											<!-- Card -->
-										</div>
-
-										<div class="col-sm-4">
-											<!-- Card -->
-											<div class="card mb-4">
-												<!--Card content-->
-												<div class="card-body">
-
-													<!--Title-->
-													<h4 class="card-title">Card title</h4>
-													<!--Text-->
-													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-													<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-													<button type="button" class="btn btn-light-blue btn-md">Read more</button>
-
-												</div>
-
-											</div>
-											<!-- Card -->
-										</div>
-
-										<div class="col-sm-4">
-											<!-- Card -->
-											<div class="card mb-4">
-												<!--Card content-->
-												<div class="card-body">
-
-													<!--Title-->
-													<h4 class="card-title">Card title</h4>
-													<!--Text-->
-													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-													<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-													<button type="button" class="btn btn-light-blue btn-md">Read more</button>
-
-												</div>
-
-											</div>
-											<!-- Card -->
-										</div>
-
 
 									</div>
 								</div>
@@ -384,7 +287,7 @@
 	</main>
 
 	<!-- Footer -->
-	<footer class="page-footer font-small unique-color-dark mt-5 footer ">
+	<footer class="page-footer font-small unique-color-dark mt-5 footer text-center">
 
 		<!-- Footer Elements -->
 		<div class="container">
@@ -395,7 +298,7 @@
 					<h5 class="mb-1">Register for free</h5>
 				</li>
 				<li class="list-inline-item">
-					<a href="#!" class="btn btn-outline-white btn-rounded">Return to top</a>
+					<a href="#topSection" class="btn btn-outline-white btn-rounded">Return to top</a>
 				</li>
 			</ul>
 			<!-- Call to action -->
