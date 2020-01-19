aecbb917f7bf761cc0648dbcf67c596b946a6ae8
Jabez123 <it.jabez16@gmail.com>
2020-01-19 11:29:25 +0800

    login module added

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
-include("config/connectDatabase.php");
-include("config/connectServer.php");
-
+// Initialize the session
 session_start();
 
-if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-	header("location: index.php");
-	exit();
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+	header("location: login.php");
+	exit;
 }
 ?>
 <!DOCTYPE html>
--- login.php
+++ login.php
@@ -1,13 +1,100 @@
 <?php 
-include("config/connectDatabase.php");
-include("config/connectServer.php");
 
+// Initialize the session
 session_start();
-
-if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-	header("location: index.php");
-	exit();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: index.php");
+    exit;
 }
+
+// Include configs
+require_once("config/connectServer.php");
+require_once("config/connectDatabase.php");
+
+
+$username = "";
+$password = "";
+$username_error = "";
+$password_error = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	// Check username if empty
+	if (empty(trim($_POST['username']))) {
+		$username_error = "Please enter username";
+	}
+	else {
+		$username = trim($_POST["username"]);
+	}
+
+	// Check if password is empty
+	if(empty(trim($_POST["password"]))){
+		$password_error = "Please enter your password.";
+	} 
+	else{
+		$password = trim($_POST["password"]);
+	}
+
+    // Validate credentials
+	if(empty($username_error) && empty($password_error)) {
+        // Prepare a select statement
+		$sql = "SELECT admin_id, username, password FROM admin_tb WHERE username = ?";
+		if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+			$param_username = $username;
+
+            // Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+                // Store result
+				mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+				if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+					mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+					if(mysqli_stmt_fetch($stmt)){
+						if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+							session_start();
+
+                            // Store data in session variables
+							$_SESSION["loggedin"] = true;
+							$_SESSION["id"] = $id;
+							$_SESSION["username"] = $username;                         
+
+                            // Redirect user to index page
+							header("location: index.php");
+						} else{
+                            // Display an error message if password is not valid
+							$password_error = "The password you entered was not valid.";
+						}
+					}
+				} else{
+                    // Display an error message if username doesn't exist
+					$username_error = "No account found with that username.";
+				}
+			} else{
+				echo "Oops! Something went wrong. Please try again later.";
+			}
+
+			// Close statement
+			mysqli_stmt_close($stmt);
+		}
+		else {
+			echo "Statement Problem: " . mysqli_error($conn);
+		}
+	}
+
+	// Close connection
+	mysqli_close($conn);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,18 +122,24 @@ if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 					<div class="card-body">
 						<!-- Material form group -->
-						<form action="#" method="post">
+						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 							<!-- Material input -->
-							<div class="md-form form-group mt-5">
+							<div class="md-form form-group mt-5 
+							<?php echo (!empty($username_error)) ? 'has-error' : ''; ?>">
 								<i class="fas fa-user prefix"></i>
-								<input type="text" id="username" class="form-control">
+								<input type="text" name="username" id="username" class="form-control" value="<?php echo $username; ?>">
 								<label for="username">Username</label>
+
+								<span class="help-block"><?php echo $username_error; ?></span>
 							</div>
 							<!-- Material input -->
-							<div class="md-form form-group mt-5">
+							<div class="md-form form-group mt-5 
+							<?php echo (!empty($password_error)) ? 'has-error' : ''; ?>">
 								<i class="fas fa-lock prefix"></i>
-								<input type="password" id="password" class="form-control">
+								<input type="password" name="password" id="password" class="form-control">
 								<label for="password">Password</label>
+
+								<span class="help-block"><?php echo $password_error; ?></span>
 							</div>
 
 							<!-- Default checked -->
@@ -57,7 +150,7 @@ if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 							<div class="text-center">
 								<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-								<button type="button" class="btn btn-primary">Primary</button>
+								<button type="submit" value="Login" class="btn btn-primary">Log in</button>
 							</div>
 						</form>
 						<!-- Material form group -->
