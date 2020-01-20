0f214b029dd94670487ef682e6cb12856eda781c
Jabez123 <it.jabez16@gmail.com>
2020-01-20 18:18:40 +0800

    department page added, fix active hyperlink in trainee.php

--- department.php
+++ department.php
@@ -1,3 +1,4 @@
+
 <?php 
 // Initialize the session
 session_start();
@@ -36,10 +37,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 						<li class="nav-item">
 							<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 						</li>
-						<li class="nav-item active">
+						<li class="nav-item">
 							<a class="nav-link" href="trainee.php">Trainees</a>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item active">
 							<a class="nav-link" href="department.php">Departments</a>
 						</li>
 					</ul>
--- index.php
+++ index.php
@@ -28,41 +28,41 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="trainee.php">Trainees</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="department.php">Departments</a>
-					</li>
-				</ul>
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<span class="navbar-text white-text mr-5">
-							Hello <?php echo htmlspecialchars($_SESSION["username"]); ?>!
-						</span>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-						aria-haspopup="true" aria-expanded="false">
-						<i class="fas fa-user"></i>
-					</a>
-					<div class="dropdown-menu dropdown-menu-left"
-					aria-labelledby="navbarDropdownMenuLink-333">
-					<a class="dropdown-item" href="change_password.php">Change Password</a>
-					<a class="dropdown-item" href="logout.php">Logout</a>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="trainee.php">Trainees</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="department.php">Departments</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<span class="navbar-text white-text mr-5">
+								Hello <?php echo htmlspecialchars($_SESSION["username"]); ?>!
+							</span>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false">
+							<i class="fas fa-user"></i>
+							</a>
+							<div class="dropdown-menu dropdown-menu-left"
+								aria-labelledby="navbarDropdownMenuLink-333">
+								<a class="dropdown-item" href="change_password.php">Change Password</a>
+								<a class="dropdown-item" href="logout.php">Logout</a>
+							</div>
+						</li>
+					</ul>
 				</div>
-			</li>
-		</ul>
-	</div>
-</div>
-</nav>
-</header>
+			</div>
+		</nav>
+	</header>
 
 <main class="mt-5">
 
