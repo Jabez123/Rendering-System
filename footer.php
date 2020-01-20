19c516f8ebd5af91bbaacd787b9fb43015e6a341
Jabez123 <it.jabez16@gmail.com>
2020-01-20 21:52:00 +0800

    footer.php added

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!-- Footer -->
+	<footer class="page-footer font-small unique-color-dark mt-5 footer text-center">
+
+		<!-- Footer Elements -->
+		<div class="container">
+
+			<!-- Call to action -->
+			<ul class="list-unstyled list-inline py-2">
+				<li class="list-inline-item">
+					<h5 class="mb-1">Register for free</h5>
+				</li>
+				<li class="list-inline-item">
+					<a href="#topSection" class="btn btn-outline-white btn-rounded">Return to top</a>
+				</li>
+			</ul>
+			<!-- Call to action -->
+
+		</div>
+		<!-- Footer Elements -->
+
+		<!-- Copyright -->
+		<div class="footer-copyright text-center py-3">© 2020 Copyright:
+			<a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+		</div>
+		<!-- Copyright -->
+
+	</footer>
+	<!-- Footer -->
+
+
+<script type="text/javascript" src="dist/js/jquery.js"></script>
+<script type="text/javascript" src="dist/js/popper.js"></script>
+<script type="text/javascript" src="dist/js/bootstrap.js"></script>
+<script type="text/javascript" src="dist/js/mdb.js"></script>
+<!-- MDBootstrap Datatables  -->
+<script type="text/javascript" src="dist/js/addons/datatables.min.js"></script>
+<script type="text/javascript" src="dist/js/other.js"></script>
